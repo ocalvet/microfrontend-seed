6e907d390893491c6bce7449ef3abd028d4f854e
Ovidio R. Calvet <calveto@gmail.com>
2019-06-16 09:31:21 -0400

    Install create-react-app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Sample application to test microfrontends</h1>
+    </div>
+  );
+}
+
+export default App;
