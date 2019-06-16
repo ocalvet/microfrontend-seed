b8704f2fc10b3bf43db63bca2dd07b2a538da085
Ovidio R. Calvet <calveto@gmail.com>
2019-06-16 10:20:16 -0400

    Add communication from application to parent frame

--- src/ApplicationLoader/ApplicationLoader.js
+++ src/ApplicationLoader/ApplicationLoader.js
@@ -1,18 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Menu = ({ app }) => {
+  const [imagesCount, setCount] = useState(0);
+  window.onmessage = function(e) {
+    // console.log('Application message', e);
+    if (e.data && e.data.type) {
+      switch (e.data.type) {
+        case 'dog.image.changed':
+          setCount(prevCount => prevCount + 1);
+          return;
+        default:
+          console.log('UNKNOWN EVENT', e.data);
+          return;
+      }
+    }
+  };
   return (
-    // Sites that can be display on the iFrame cannot  have the If trying to use any external site the
-    // response headers cannot be X-Frame-Options: SAMEORIGIN
-    <iframe
-      style={{
-        border: 'none',
-        width: '100%',
-        height: '1200px'
-      }}
-      title="application"
-      src={app.link}
-    />
+    <>
+      <div>Dog images {imagesCount}</div>
+      {/* Sites that can be display on the iFrame cannot have the If trying to
+      use any external site the response headers cannot be X-Frame-Options: SAMEORIGIN */}
+      <iframe
+        style={{
+          border: 'none',
+          width: '100%',
+          height: '1200px'
+        }}
+        title="application"
+        src={app.link}
+      />
+    </>
   );
 };
 
